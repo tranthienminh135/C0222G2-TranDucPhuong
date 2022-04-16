6e5b42d1ea6742c7cac328f314ba1f881f8f3d03
tranducphuong <tranthienminh135>
2022-04-16 08:53:31 +0700

    update bai tap

--- module2/src/ss10_stack_queue/bai_tap/ss4_palindrome/Palindrome.java
+++ module2/src/ss10_stack_queue/bai_tap/ss4_palindrome/Palindrome.java
@@ -7,26 +7,28 @@ import java.util.Stack;
 public class Palindrome {
     public static void main(String[] args) {
         String str = "Able was I ere I saw Elba";
+        boolean flag = false;
 
         Stack<String> stack = new Stack<>();
         Queue<String> queue = new ArrayDeque<>();
 
         for (int i = 0; i < str.length(); i++) {
             stack.push(str.charAt(i) + "");
-        }
-
-        for (int i = 0; i < str.length(); i++) {
-            System.out.print(stack.pop());
-        }
-
-        System.out.println();
-
-        for (int i = 0; i < str.length(); i++) {
             queue.offer(str.charAt(i)+ "");
         }
 
         for (int i = 0; i < str.length(); i++) {
-            System.out.print(queue.poll());
+            if (stack.pop().equalsIgnoreCase(queue.poll())) {
+                flag = true;
+            } else {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            System.out.println("\"" + str + "\" is palindrome!");
+        } else {
+            System.out.println("\"" + str + "\" is NOT palindrome!");
         }
     }
 }
