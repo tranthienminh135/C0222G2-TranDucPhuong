f0355013fcb3f508aea282d92aba67b4edf4aa5c
tranthienminh135 <67372816+tranthienminh135@users.noreply.github.com>
2022-07-19 20:46:33 +0700

    update

--- module5/ss2_angular/bai_tap/angular-color-picker-app/src/app/angular-color-picker-app/angular-color-picker-app.component.ts
+++ module5/ss2_angular/bai_tap/angular-color-picker-app/src/app/angular-color-picker-app/angular-color-picker-app.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./angular-color-picker-app.component.css']
 })
 export class AngularColorPickerAppComponent implements OnInit {
- color = '';
- setColor(color) {
-   this.color = color;
+ color1 = 0;
+ color2 = 0;
+ color3 = 0;
+ setColor1(color1) {
+   this.color1 = color1;
  }
+  setColor2(color2) {
+    this.color2 = color2;
+  }
+  setColor3(color3) {
+    this.color3 = color3;
+  }
   constructor() { }
 
   ngOnInit(): void {
